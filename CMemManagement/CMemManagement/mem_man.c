c8b0a604d028389313de762565741ee6b9614a72
ToastyTad <toastytadgit@gmail.com>
2018-01-26 14:49:07 -0500

    Project and function skeletons added.

---
+++ CMemManagement/CMemManagement/mem_man.c
@@ -0,0 +1,176 @@
+#include <stdio.h>
+
+void DemonstratePointersAndNormalVariables();
+void DemonstratePointerSizeComparison();
+void DemonstratePointerAsArgument();
+void DemonstrateArrayAsArgument();
+void DemonstratePointerAsReturn();
+void DemonstratePointerArithmetic();
+void DemonstratePointerAsArray();
+void DemonstrateArrayAsPointer();
+void DemonstrateMalloc();
+void DemonstrateCalloc();
+void DemonstrateRealloc();
+void DemonstrateDeallocation();
+void DemonstrateLinkedList();
+void DemonstratePointerToPointer();
+void DemonstratePointerToFunction();
+void DemonstrateRestrictPointer();
+void DemonstrateFlexibleArray();
+
+int main()
+{
+	
+
+	system("pause");
+	return 0;
+}
+
+//*** Demonstrating pointer and normal variable
+//*** similarities and differences.
+
+void DemonstratePointersAndNormalVariables()
+{
+
+}
+
+
+//*** Demonstrating size comparison between different
+//*** pointers and relating normal types.
+
+void DemonstratePointerSizeComparison()
+{
+
+}
+
+
+//*** Demonstrating the use of pointers as arguments
+//*** for functions.
+
+void DemonstratePointerAsArgument()
+{
+
+}
+
+
+//*** Demonstrating the use of arrays as arguments
+//*** for functions.
+
+void DemonstrateArrayAsArgument()
+{
+
+}
+
+
+//*** Demonstrating functions that return a pointer,
+//*** not a normal variable.
+
+void DemonstratePointerAsReturn()
+{
+
+}
+
+
+//*** Demonstrating pointer arithmetic.
+
+void DemonstratePointerArithmetic()
+{
+
+}
+
+
+//*** Demonstrating the use of pointer variables
+//*** as an array.
+
+void DemonstratePointerAsArray()
+{
+
+}
+
+
+//*** Demonstrating the use of array names as
+//*** pointers.
+
+void DemonstrateArrayAsPointer()
+{
+
+}
+
+
+//*** Demonstrating the use of the stdlib function
+//*** malloc and related topics.
+
+void DemonstrateMalloc()
+{
+
+}
+
+
+//*** Demonstrating the use of the stdlib function
+//*** calloc and related topics.
+
+void DemonstrateCalloc()
+{
+
+}
+
+
+//*** Demonstrating the use of the stdlib function
+//*** realloc and related topics.
+
+void DemonstrateRealloc()
+{
+
+}
+
+
+//*** Demonstrating the deallocation of memory in
+//*** the C programming language. Also covering
+//*** some other forms of memory allocation.
+
+void DemonstrateDeallocation()
+{
+
+}
+
+
+//*** Demonstrating linked lists and some other
+//*** basic data structures.
+
+void DemonstrateLinkedList()
+{
+
+}
+
+
+//*** Demonstrating pointers to other pointers.
+
+void DemonstratePointerToPointer()
+{
+
+}
+
+
+//*** Demonstrating pointers to functions.
+
+void DemonstratePointerToFunction()
+{
+
+}
+
+
+//*** Demonstrating the use of the keyword
+//*** 'restrict' and its effect on pointers.
+
+void DemonstrateRestrictPointer()
+{
+
+}
+
+
+//*** Demonstrating flexible array members.
+
+void DemonstrateFlexibleArray()
+{
+
+}
